184df2ba07fa35e1accdaac5f9122193b90e54da
Piotr Turlej <piotr.turlej@me.com>
2017-10-02 19:04:13 +0200

    tests scripts configured

--- Tests/apiController.test.js
+++ Tests/apiController.test.js
@@ -1,5 +1,34 @@
 const expect = require('expect')
 const request = require('supertest')
 const {app} = require('../server')
+const {User} = require('../Models/User')
 
+
+describe('POST /user', () => {
+    it('Should create new User', (done) => {
+       
+        var user =  { 
+           name: 'Piotr',
+           email: 'piotr@wp.pl'
+        }
+
+        request(app).post('/user').send(user)
+        .expect(200)
+        .expect((response) => {
+            expect(res.body.name).toBe(user.name)
+            expect(res.body.email).toBe(user.email)
+        })
+        .end((error, response) => {
+            if (error) {
+                return done(error)
+            }
+            User.find().then((users) => {
+                expect(users.length).toBe(1)
+                expect(users[0].name).toBe(user.name)
+                expect(user[0].email).toBe(user.email)
+                done()
+            }).catch((error) => done(error))
+        })
+    })
+})
 
