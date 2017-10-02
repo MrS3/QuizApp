c87f62559cec449f9399cf8d75d4a9f4c662757a
Piotr Turlej <piotr.turlej@me.com>
2017-10-02 19:42:14 +0200

    create user test

--- Tests/apiController.test.js
+++ Tests/apiController.test.js
@@ -3,20 +3,21 @@ const request = require('supertest')
 const {app} = require('../server')
 const {User} = require('../Models/User')
 
+beforeEach((done) => {
+    User.remove({}).then(() => done())
+})
 
 describe('POST /user', () => {
     it('Should create new User', (done) => {
-       
-        var user =  { 
-           name: 'Piotr',
-           email: 'piotr@wp.pl'
+        var user = { 
+           name: 'Janek',
+           email: 'janek@02.pl'
         }
-
+        
         request(app).post('/user').send(user)
         .expect(200)
         .expect((response) => {
-            expect(res.body.name).toBe(user.name)
-            expect(res.body.email).toBe(user.email)
+            expect(response.body.email).toBe(user.email)
         })
         .end((error, response) => {
             if (error) {
@@ -24,8 +25,7 @@ describe('POST /user', () => {
             }
             User.find().then((users) => {
                 expect(users.length).toBe(1)
-                expect(users[0].name).toBe(user.name)
-                expect(user[0].email).toBe(user.email)
+                expect(users[0].email).toBe(user.email)
                 done()
             }).catch((error) => done(error))
         })
